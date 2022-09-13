8b0ae2ddd7807ac32e53e59827a31f534ce02ae5
Kennerson Pereira <kennerson.pereira@thoughtworks.com>
2022-09-13 12:12:51 -0300

    first commit

---
+++ LambdaExpressions/BookRepository.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace LambdaExpressions
+{
+    public class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>()
+            {
+                new Book() { Title = "Book 1", Price = 5 },
+                new Book() { Title = "Book 2", Price = 7 },
+                new Book() { Title = "Book 3", Price = 17 }
+            };
+        }
+        
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //What are lambdas?
+            //No access modifier (public, private...)
+            //No name
+            //No return statement
+            
+            //args => expression
+            //number => number*number
+            Func<int, int> square = number => number * number;
+
+            Console.WriteLine(square(5));
+            
+            // () => ...
+            // x => ...
+            // (x,y,z) =>
+
+            const int factor = 5;
+
+            Func<int, int> multiplier = n => n * factor;
+            var result = multiplier(10);
+
+            Console.WriteLine(result);
+
+            Console.WriteLine(Square(5));
+            
+            //-----------------------------------------------
+
+            var books = new BookRepository().GetBooks();
+            var cheapBooks = books.FindAll(book => book.Price < 10);
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
